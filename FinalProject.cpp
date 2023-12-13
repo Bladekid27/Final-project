50ea3b4abcf1fd032e1f58e97ec065b5397bda29
Bladekid27 <148721801+Bladekid27@users.noreply.github.com>
2023-12-13 16:59:04 -0500

    Update FinalProject.cpp
    total calculation has been added its still a work in progress

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -7,42 +7,78 @@
 #include <iomanip>
 
 using namespace std;
-
+const int STATES = 50;
 void Introduction(char c, string message)
 {
     cout << setfill(c) << setw(50) << ' ' << endl;
     cout << setw(45) << message << setw(5) << ' ' << endl;
     cout << setfill(c) << setw(50) << ' ' << endl;
-    
+
 }
 
 //Loading the array with state names from a file
-bool readFile(string fileName, string states[])
+bool readFile(string fileName, string Electoral, int e[], string states[])
 {
     ifstream statesData(fileName);
-
-    if (!statesData)
+    ifstream electoral(Electoral);
+    if (!statesData && electoral)
+    {
         return false;
-
+    }
     for (int i = 0; i < 50; i++)
     {
         getline(statesData, states[i]);
-        
+        electoral >> e[i];
         // Test to see if the data was loaded correctly. 
         //cout << states[i] << endl;
     }
     return true;
 }
-
-
-int main(int argc, char* argv[])
+// This will find the state you want and add it to another array then remove it from the electoral array
+// current issues are states with spaces so New York
+void total(int e[], string s[],int u[], int& total)
 {
-    string stateList[50];
+    string choice;
+    int count = 0;
+    cin >> choice;
+    for (int line = 0; line < STATES; line++)
+    {
+        if (s[line] == choice)
+        {
+            line += STATES;
+        }
+        else
+        {
+            count++;
+        }
+    }
 
-    Introduction('*', "Welcome to the 2024 Presidential Election");
-    
-    readFile("ListofStates.txt", stateList);
+    total += e[count];
+    u[count] += e[count];
+    e[count] = 0;
 
 }
 
+int main(int argc, char* argv[])
+{
+    int Total = 0;
+    string choice;
+    string Ttotal = "T";
+    const string QUIT = "Q";
+    string StateList[STATES] = {};
+    int Electoral[STATES] = {};
+    int UserTotal[STATES] = {};
+    Introduction('*', "Welcome to the 2024 Presidential Election");
+    readFile("ListofStates.txt", "electoral.txt", Electoral, StateList);
+    //This will let you choose the operation
+    while (choice != QUIT)
+    {
+        cout << "Please make a selection uppercase only ", cin >> choice;
+      
+        if (choice == Ttotal)
+        {
+            total(Electoral, StateList,UserTotal, Total);
+        }
+    }
+}
 
