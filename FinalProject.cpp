1e677b2abaf97478d4acddec9e05bf1135b22f1c
Bladekid27 <148721801+Bladekid27@users.noreply.github.com>
2023-12-13 14:43:25 -0500

    Update FinalProject.cpp
    This adds the file extract for the electoral file

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -7,27 +7,28 @@
 #include <iomanip>
 
 using namespace std;
-
+const int STATES = 50;
 void Introduction(char c, string message)
 {
     cout << setfill(c) << setw(50) << ' ' << endl;
     cout << setw(45) << message << setw(5) << ' ' << endl;
     cout << setfill(c) << setw(50) << ' ' << endl;
-    
+
 }
 
 //Loading the array with state names from a file
-bool readFile(string fileName, string states[])
+bool readFile(string fileName, string Electoral, int e[], string states[])
 {
     ifstream statesData(fileName);
-
-    if (!statesData)
+    ifstream electoral(Electoral);
+    if (!statesData && electoral)
+    {
         return false;
-
+    }
     for (int i = 0; i < 50; i++)
     {
         getline(statesData, states[i]);
-        
+        electoral >> e[i];
         // Test to see if the data was loaded correctly. 
         //cout << states[i] << endl;
     }
@@ -37,12 +38,15 @@ bool readFile(string fileName, string states[])
 
 int main(int argc, char* argv[])
 {
-    string stateList[50];
 
+    string stateList[STATES] = {};
+    int Electoral[STATES] = {};
     Introduction('*', "Welcome to the 2024 Presidential Election");
-    
-    readFile("ListofStates.txt", stateList);
+
+    readFile("ListofStates.txt","electoral.txt",Electoral, stateList);
 
 }
+
+
 
 
