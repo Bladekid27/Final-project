d58dc0bf36f3b3cb5daad48c3db4b5ee9cd69405
Bladekid27 <148721801+Bladekid27@users.noreply.github.com>
2023-12-14 16:52:06 -0500

    Update FinalProject.cpp
    I added the other options to see the user and opponent total. The program is almost finished the intro needs to be finished listing the options of how to access each function. There's still the issue with the state selection that I need to fix.

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -13,7 +13,7 @@ void Introduction(char c, string message)
     cout << setfill(c) << setw(50) << ' ' << endl;
     cout << setw(45) << message << setw(5) << ' ' << endl;
     cout << setfill(c) << setw(50) << ' ' << endl;
-
+    // The options in the while list need to be added
 }
 
 //Loading the array with state names from a file
@@ -34,13 +34,11 @@ bool readFile(string fileName, string Electoral, int e[], string states[])
     }
     return true;
 }
-// This will find the state you want and add it to another array then remove it from the electoral array
-// current issues are states with spaces so New York
-void total(int e[], string s[],int u[], int& total)
+void SelectState(int e[], string s[],int u[])
 {
     string choice;
     int count = 0;
-    cin >> choice;
+    cout << "Please select a state every state starts with a capital letter ", cin >> choice;
     for (int line = 0; line < STATES; line++)
     {
         if (s[line] == choice)
@@ -53,32 +51,88 @@ void total(int e[], string s[],int u[], int& total)
         }
     }
 
-    total += e[count];
     u[count] += e[count];
     e[count] = 0;
 
 }
+void Total_Electoral(int e[], string s[], int u[])
+{
+    int Points_User = {};
+    int Oponnents_Points = {};
+    int Total = {};
+    for (int row = 0; row < STATES; row++)
+    {
+        Points_User += u[row];
+        Oponnents_Points += e[row];
+    }
 
+    if (Points_User > Oponnents_Points)
+    {
+        Total += Points_User - Oponnents_Points;
+        cout << "You've won with a total of " << Points_User << " Your in the lead by " << Total << " Your opponent is at " << Oponnents_Points << endl;
+    }
+    else if (Points_User < Oponnents_Points)
+    {
+        Total += Oponnents_Points - Points_User;
+        cout << "You lossed with a total of " << Points_User << " You've lost by " << Total << " Your opponent is at " << Oponnents_Points << endl;
+    }
+    else
+    {
+        cout << "You are currently tied you have " << Points_User <<" Your opponent has " << Oponnents_Points << endl;
+    }
+}
+void User_Total(int u[])
+{
+    int Total = {};
+    for (int row = 0; row < STATES; row++)
+    {
+        Total += u[row];
+    }
+    cout << "You currently have a total of " << Total << endl;
+}
+
+void OTotal(int e[])
+{
+    int Total = {};
+    for (int row = 0; row < STATES; row++)
+    {
+        Total += e[row];
+    }
+    cout << "Your opponent currently has a total of " << Total << endl;
+}
 int main(int argc, char* argv[])
 {
-    int Total = 0;
     string choice;
     string Ttotal = "T";
+    string StateChoice = "S";
+    string Opponent = "O";
     const string QUIT = "Q";
     string StateList[STATES] = {};
     int Electoral[STATES] = {};
     int UserTotal[STATES] = {};
     Introduction('*', "Welcome to the 2024 Presidential Election");
     readFile("ListofStates.txt", "electoral.txt", Electoral, StateList);
-    //This will let you choose the operation
     while (choice != QUIT)
     {
         cout << "Please make a selection uppercase only ", cin >> choice;
       
-        if (choice == Ttotal)
+        if (choice == StateChoice)
         {
-            total(Electoral, StateList,UserTotal, Total);
+            SelectState(Electoral, StateList,UserTotal);
+        }
+        else if (choice == QUIT)
+        {
+            Total_Electoral(Electoral, StateList, UserTotal);
+        }
+        else if (choice == Ttotal)
+        {
+            User_Total(UserTotal);
+        }
+        else if (choice == Opponent)
+        {
+            OTotal(Electoral);
         }
     }
 }
+
 
